8a462e0dd4b07d087161a6751485be79a8367ce0
Charlottes84 <82307341+Charlottes84@users.noreply.github.com>
2024-09-17 09:42:52 +0700

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,8 @@
+// Import your library
+// Do not change the name of the Solution class
+public class Solution {
+    // Type your main code here
+  public static void main (String[] args) {
+    System.out.println("Hello World");
+  }
+}
